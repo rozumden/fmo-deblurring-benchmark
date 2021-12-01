c1ad0d4517a1097096e5934199ee3b63368b40d6
Denys Rozumnyi <denysr@eu-login-01.euler.ethz.ch>
2021-12-01 14:21:04 +0100

    add Kornia

--- example_defmo_source.py
+++ example_defmo_source.py
@@ -5,8 +5,14 @@ from benchmark.benchmark_loader import *
 from benchmark.loaders_helpers import *
 import argparse
 
-from kornia.feature import DeFMO
-from torchvision import transforms
+import sys
+sys.path.insert(0, './DeFMO')
+from models.encoder import *
+from models.rendering import *
+from dataloaders.loader import get_transform
+from helpers.torch_helpers import renders2traj
+
+g_saved_models_folder = './DeFMO/saved_models/'
 
 def parse_args():
 	parser = argparse.ArgumentParser()
@@ -28,20 +34,34 @@ def main():
 	device = torch.device("cuda:{}".format(gpu_id) if torch.cuda.is_available() else "cpu")
 	print(device)
 	torch.backends.cudnn.benchmark = True
-	
-	defmo = DeFMO(pretrained=True).to(device)
-	defmo.train(False)
+	encoder = EncoderCNN()
+	rendering = RenderingCNN()
+
+	if torch.cuda.is_available():
+		encoder.load_state_dict(torch.load(os.path.join(g_saved_models_folder, 'encoder_best.pt')))
+		rendering.load_state_dict(torch.load(os.path.join(g_saved_models_folder, 'rendering_best.pt')))
+	else:
+		encoder.load_state_dict(torch.load(os.path.join(g_saved_models_folder, 'encoder_best.pt'),map_location=torch.device('cpu')))
+		rendering.load_state_dict(torch.load(os.path.join(g_saved_models_folder, 'rendering_best.pt'),map_location=torch.device('cpu')))
+		
+	encoder = encoder.to(device)
+	rendering = rendering.to(device)
+
+	encoder.train(False)
+	rendering.train(False)
 
 	def deblur_defmo(I,B,bbox_tight,nsplits,radius,obj_dim):
 		bbox = extend_bbox(bbox_tight.copy(),4*np.max(radius),g_resolution_y/g_resolution_x,I.shape)
 		im_crop = crop_resize(I, bbox, (g_resolution_x, g_resolution_y))
 		bgr_crop = crop_resize(B, bbox, (g_resolution_x, g_resolution_y))
-		input_batch = torch.cat((transforms.ToTensor()(im_crop), transforms.ToTensor()(bgr_crop)), 0).to(device).unsqueeze(0).float()
-		defmo.rendering.tsr_steps = nsplits*multi_f+1
-		defmo.rendering.times = torch.linspace(0,1,defmo.rendering.tsr_steps)
+		preprocess = get_transform()
+		input_batch = torch.cat((preprocess(im_crop), preprocess(bgr_crop)), 0).to(device).unsqueeze(0).float()
 		with torch.no_grad():
-			renders = defmo(input_batch)
+			latent = encoder(input_batch)
+			times = torch.linspace(0,1,nsplits*multi_f+1).to(device)
+			renders = rendering(latent,times[None])
 			renders = renders[:,:-1].reshape(1, nsplits, multi_f, 4, g_resolution_y, g_resolution_x).mean(2) # add small motion blur 
+
 		renders_rgba = renders[0].data.cpu().detach().numpy().transpose(2,3,1,0)
 		est_hs_crop = rgba2hs(renders_rgba, bgr_crop)
 		est_hs = rev_crop_resize(est_hs_crop,bbox,I)
@@ -52,6 +72,7 @@ def main():
 	args.add_traj = False	
 	args.method_name = 'DeFMO'
 	run_benchmark(args, deblur_defmo)
+
 
 if __name__ == "__main__":
     main()
